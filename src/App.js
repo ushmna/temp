9d2f9cf6a92c48dae63babf05ea645559ce1020b
ushma <ushmakhalid1@gmail.com>
2022-07-28 12:36:23 -0700

    project structure managed

--- src/App.js
+++ src/App.js
@@ -1,37 +1,17 @@
-import './App.css';
+import "./App.css";
+import SideNavbar from "./Components/SideNavbar";
 
 function App() {
   return (
-    <>
-<div class="flex flex-row h-screen">
-  <div class="basis-1/7 border-r-2">  
-  <div className='bg-white p-10'>
-  <img src={require("./images/vector.svg")}  class="max-w-full h-auto" alt="logo" />
-  <div className='pt-40'>
-  <ul >
-      <li ><img src={require('./images/vector.svg')} class="max-w-full h-auto" alt="Home" /></li>
-      <li className='pt-5'><img src={require('./images/vector.svg')} class="max-w-full h-auto" alt="Home" /></li>
-      <li className='pt-5'><img src={require('./images/vector.svg')} class="max-w-full h-auto" alt="Home" /></li>
-      <li className='pt-5'><img src={require('./images/vector.svg')} class="max-w-full h-auto" alt="Home" /></li>
-      <li className='pt-5'><img src={require('./images/vector.svg')} class="max-w-full h-auto" alt="Home" /></li>
-    </ul>
-  </div>
-<div className='absolute bottom-0 pb-10 pr-10'>
-<img src={require('./images/vector.svg')} class="max-w-full h-auto" alt="settings" />
-</div>
+    <div className="w-full flex">
+      <SideNavbar/>
+      <div className="bg-red-200 w-full flex">
+        
+        <div className="bg-yellow-200 w-full bg-textColor">1</div>
+        <div className="bg-green-200 w-full">2</div>
+      </div>
  
-  
     </div>
-    </div>
-  <div class="basis-1/2 bg-slate-100">02</div>
-  <div class="basis-1/2 bg-slate-100">03</div>
-</div>
-
-    
-
-    
-    </>
-    
   );
 }
 
